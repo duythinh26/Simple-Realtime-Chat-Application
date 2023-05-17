5fe170a17f45002c2e115d58ec337671f0c8c76e
duythinh26 <pduythinh79@gmail.com>
2023-05-17 10:42:13 +0700

    feat: main.go

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		fmt.Printf("Couldn't initialize database connection: %s", err)
 	}
+
+	userRepo := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRepo)
+	userHandler := user.NewHandler(userService)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8000")
 }
